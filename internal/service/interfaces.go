d39af440c6c2cc72f251b66c49d63e04d6741be3
Alex322322 <alex9999091@gmail.com>
2026-05-18 22:14:18 +0300

    feat: add service interfaces and  user service

---
+++ internal/service/interfaces.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Alex322322/bhpegram/internal/domain"
+)
+
+type UserService interface {
+	Create(ctx context.Context, user *domain.CreateUserRequest) (*domain.User, error)
+	GetByID(ctx context.Context, id int64) (*domain.User, error)
+	GetByUsername(ctx context.Context, username string) (*domain.User, error)
+	GetByEmail(ctx context.Context, email string) (*domain.User, error)
+	Update(ctx context.Context, user *domain.User) (*domain.User, error)
+	Delete(ctx context.Context, id int64) error
+}
+
+type ChatService interface {
+	Create(ctx context.Context, chat *domain.Chat) (*domain.Chat, error)
+	GetByID(ctx context.Context, id int64) (*domain.Chat, error)
+	GetUserChats(ctx context.Context, userID int64) ([]*domain.Chat, error)
+	AddMember(ctx context.Context, member *domain.ChatMember) error
+	RemoveMember(ctx context.Context, chatID int64, userID int64) error
+	Update(ctx context.Context, chat *domain.Chat) (*domain.Chat, error)
+	Delete(ctx context.Context, id int64) error
+}
+
+type MessageService interface {
+	Create(ctx context.Context, message *domain.Message) (*domain.Message, error)
+	GetByID(ctx context.Context, id int64) (*domain.Message, error)
+	Update(ctx context.Context, message *domain.Message) (*domain.Message, error)
+	Delete(ctx context.Context, id int64) error
+	AddReaction(ctx context.Context, reaction *domain.MessageReaction) error
+	UpdateReaction(ctx context.Context, reaction *domain.MessageReaction) error
+	DeleteReaction(ctx context.Context, reaction *domain.MessageReaction) error
+	GetReactions(ctx context.Context, id int64) ([]*domain.MessageReaction, error)
+	GetRead(ctx context.Context, id int64) ([]*domain.MessageRead, error)
+	// beforeID is used for cursor-based pagination, it returns messages with ID less than beforeID
+	GetMessages(ctx context.Context, chatID int64, beforeID int64, limit int) ([]*domain.Message, error)
+}
---
+++ internal/service/user_service.go
@@ -0,0 +1,93 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/Alex322322/bhpegram/internal/domain"
+	"github.com/Alex322322/bhpegram/internal/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var _ UserService = (*userService)(nil)
+
+type userService struct {
+	repo repository.UserRepository
+}
+
+func NewAppUserService(repo repository.UserRepository) *userService {
+	return &userService{repo: repo}
+}
+
+func (s *userService) Create(ctx context.Context, user *domain.CreateUserRequest) (*domain.User, error) {
+	u, err := s.repo.GetByEmail(ctx, user.Email)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to check email: %w", err)
+	}
+	if u != nil {
+		return nil, fmt.Errorf("failed to create user, user with this email already exists")
+	}
+
+	u, err = s.repo.GetByUsername(ctx, user.Username)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to check username: %w", err)
+	}
+	if u != nil {
+		return nil, fmt.Errorf("failed to create user, user with this username already exists")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed generate password hash: %w", err)
+	}
+
+	user.Password = string(hash)
+	u, err = s.repo.Create(ctx, user)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return u, nil
+}
+
+func (s *userService) GetByID(ctx context.Context, id int64) (*domain.User, error) {
+	u, err := s.repo.GetByID(ctx, id)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to get user by ID: %w", err)
+	}
+	return u, nil
+}
+
+func (s *userService) GetByUsername(ctx context.Context, username string) (*domain.User, error) {
+	u, err := s.repo.GetByUsername(ctx, username)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to get user by username: %w", err)
+	}
+	return u, nil
+}
+
+func (s *userService) GetByEmail(ctx context.Context, email string) (*domain.User, error) {
+	u, err := s.repo.GetByEmail(ctx, email)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to get user by email: %w", err)
+	}
+	return u, nil
+}
+
+func (s *userService) Update(ctx context.Context, user *domain.User) (*domain.User, error) {
+	u, err := s.repo.Update(ctx, user)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("failed to update user: %w", err)
+	}
+	return u, nil
+}
+
+func (s *userService) Delete(ctx context.Context, id int64) error {
+	err := s.repo.Delete(ctx, id)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+	return nil
+}
