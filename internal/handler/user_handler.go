1bc7da184a60634946229c28319412c7c0167a31
Alex322322 <alex9999091@gmail.com>
2026-05-26 00:12:04 +0300

    feat: add register user method

--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/Alex322322/bhpegram/internal/domain"
 	"github.com/Alex322322/bhpegram/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -14,5 +15,15 @@ func NewUserHandler(service service.UserService) *UserHandler {
 }
 
 func (h *UserHandler) Register(c *gin.Context) {
-
+	var u domain.CreateUserRequest
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	_, err := h.service.Create(c.Request.Context(), &u)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+	}
+	var resp domain.UserResponse
+	c.JSON(201, gin.H{"message": "User created", "user": resp})
 }
